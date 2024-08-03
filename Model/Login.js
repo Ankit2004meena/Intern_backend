6e68c884ee668ad1a110fdf227c42d23327afa87
Ankit2004meena <ankitmeena422004@gmail.com>
2024-08-03 17:55:02 +0530

    check3

--- Model/Login.js
+++ Model/Login.js
@@ -1,13 +1,13 @@
+// server/models/Login.js
 const mongoose = require('mongoose');
 
 const loginSchema = new mongoose.Schema({
-  userId: String,
-  ip: String,
-  userAgent: String,
-  timestamp: {
-    type: Date,
-    default: Date.now,
-  },
+  userId: { type: String, required: true },
+  ip: { type: String, required: true },
+  browser: { type: String, required: true },
+  os: { type: String, required: true },
+  deviceType: { type: String, required: true },
+  timestamp: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model('Login', loginSchema);

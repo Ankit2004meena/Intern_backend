aa59d07e258e60f179fa37e17fe8b64fcfcdfcf0
Ankit2004meena <ankitmeena422004@gmail.com>
2024-08-03 19:18:57 +0530

    mob check

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,27 +1,27 @@
-// server/middleware/authentication.js
-const moment = require('moment');
+// // server/middleware/authentication.js
+// const moment = require('moment');
 
-const browserAuthentication = (req, res, next) => {
-  const { userAgent } = req.headers;
+// const browserAuthentication = (req, res, next) => {
+//   const { userAgent } = req.headers;
 
-  if (userAgent.includes('Chrome')) {
-    return res.status(401).send('OTP required'); // Or trigger OTP logic
-  }
+//   if (userAgent.includes('Chrome')) {
+//     return res.status(401).send('OTP required'); // Or trigger OTP logic
+//   }
 
-  next();
-};
+//   next();
+// };
 
-const deviceAndTimeBasedAccess = (req, res, next) => {
-  const { deviceType } = req.headers;
+// const deviceAndTimeBasedAccess = (req, res, next) => {
+//   const { deviceType } = req.headers;
 
-  if (deviceType === 'Mobile') {
-    const currentHour = moment().hour();
-    if (currentHour < 10 || currentHour > 13) {
-      return res.status(403).send('Access denied on mobile devices outside of 10 AM to 1 PM');
-    }
-  }
+//   if (deviceType === 'Mobile') {
+//     const currentHour = moment().hour();
+//     if (currentHour < 10 || currentHour > 13) {
+//       return res.status(403).send('Access denied on mobile devices outside of 10 AM to 1 PM');
+//     }
+//   }
 
-  next();
-};
+//   next();
+// };
 
-module.exports = { browserAuthentication, deviceAndTimeBasedAccess };
+// module.exports = { browserAuthentication, deviceAndTimeBasedAccess };
